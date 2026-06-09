fdc422ec6e03aa9c4b04fc864abe922792267f0a
JafetEsauWerlybi <jeckooficial@gmail.com>
2026-06-09 13:29:44 -0600

    feat: implement SAS URL generation for blob storage access

--- src/services/blobService.js
+++ src/services/blobService.js
@@ -1,5 +1,28 @@
 const { getContainerClient } = require('../config/azure');
+const { BlobSASPermissions, generateBlobSASQueryParameters } = require('@azure/storage-blob');
 const { v4: uuidv4 } = require('uuid');
+
+function generarSasUrl(blobName) {
+  const accountName = process.env.AZURE_STORAGE_CONNECTION_STRING.split('AccountName=')[1].split(';')[0];
+  const accountKey = process.env.AZURE_STORAGE_CONNECTION_STRING.split('AccountKey=')[1].split(';')[0];
+  const containerName = process.env.AZURE_STORAGE_CONTAINER;
+
+  const permissions = new BlobSASPermissions({ read: true });
+  const expiryDate = new Date();
+  expiryDate.setHours(expiryDate.getHours() + 24); // 24 horas válido
+
+  const sasQueryParams = generateBlobSASQueryParameters(
+    {
+      containerName,
+      blobName,
+      permissions,
+      expiresOn: expiryDate,
+    },
+    { accountName, accountKey }
+  ).toString();
+
+  return `https://${accountName}.blob.core.windows.net/${containerName}/${blobName}?${sasQueryParams}`;
+}
 
 async function subirAudio(buffer, mimetype) {
   const extension = mimetype.split('/')[1].replace('x-m4a', 'm4a').replace('mpeg', 'mp3');
@@ -8,12 +31,13 @@ async function subirAudio(buffer, mimetype) {
   await blockBlobClient.uploadData(buffer, {
     blobHTTPHeaders: { blobContentType: mimetype },
   });
-  return blockBlobClient.url;
+  return generarSasUrl(nombre);
 }
 
 async function eliminarAudio(url) {
-  const nombre = url.split('/').pop();
-  const blockBlobClient = getContainerClient().getBlockBlobClient(nombre);
+  const nombre = url.split('/')[0].split('.')[url.split('/')[0].split('.').length - 1];
+  const blobName = url.split('/').pop().split('?')[0];
+  const blockBlobClient = getContainerClient().getBlockBlobClient(blobName);
   await blockBlobClient.deleteIfExists();
 }
 
